96645f0be23ee5467c9e1c4c65566178448fc890
Nguyen Quoc Truong <truong.hn.1994@gmail.com>
2015-11-16 19:01:52 +0700

    Add account activations

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+     default from: "truong.hn.1994@gmail.com"
+     layout 'mailer'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,19 @@
 User.create!(name:  "Example User",
-             email: "truong@gmail.com",
-             password:              "dunghoi",
-             password_confirmation: "dunghoi",
-             admin: true)
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
